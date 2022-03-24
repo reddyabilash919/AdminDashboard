0c14d3bd023671446b05f941d26cf3cf3eee17aa
Abilash919 <reddyabilash919>
2022-03-25 09:28:42 +1100

    created all neccessary componenets

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="top">logo</div>
+        <div className="center">list</div>
+        <div className="bottom">color option</div>
+    </div>
+  )
+}
+
+export default Sidebar
