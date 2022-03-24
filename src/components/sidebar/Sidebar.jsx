be4698402108c514e1a1b5b6dab6d5f437cee589
Abilash919 <reddyabilash919>
2022-03-25 10:09:55 +1100

    mui library installed

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,11 +4,33 @@ import "./sidebar.scss";
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        <div className="top">logo</div>
-        <div className="center">list</div>
+        <div className="top">
+            <span className="logo">Admin</span>
+        </div>
+        <hr />
+        <div className="center">
+            <ul>
+                <li>
+                    <span>Dashboard</span>
+                </li>
+                <li>
+                    <span>Dashboard</span>
+                </li>
+                <li>
+                    <span>Dashboard</span>
+                </li>
+                <li>
+                    <span>Dashboard</span>
+                </li>
+                
+            </ul>
+        </div>
         <div className="bottom">color option</div>
     </div>
   )
 }
+
+
+
 
 export default Sidebar
