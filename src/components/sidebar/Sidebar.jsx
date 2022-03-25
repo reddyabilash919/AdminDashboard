347e372856121e7449062851c584d840170dcc8c
Abilash919 <reddyabilash919>
2022-03-26 06:39:12 +1100

    sidebar features added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import "./sidebar.scss";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import QueryStatsOutlinedIcon from '@mui/icons-material/QueryStatsOutlined';
+import CircleNotificationsOutlinedIcon from '@mui/icons-material/CircleNotificationsOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined';
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 const Sidebar = () => {
   return (
@@ -10,22 +22,67 @@ const Sidebar = () => {
         <hr />
         <div className="center">
             <ul>
+                <p className="title">MAIN</p>
                 <li>
+                    <DashboardIcon className="icon"/>
                     <span>Dashboard</span>
                 </li>
+                <p className="title">LISTS</p>
                 <li>
-                    <span>Dashboard</span>
+                    <PeopleOutlinedIcon className="icon"/>
+                    <span>Users</span>
                 </li>
                 <li>
-                    <span>Dashboard</span>
+                    <Inventory2OutlinedIcon className="icon"/>
+                    <span>Products</span>
                 </li>
                 <li>
-                    <span>Dashboard</span>
+                    <LibraryBooksOutlinedIcon className="icon"/>
+                    <span>Orders</span>
+                </li>
+                <li>
+                    <LocalShippingOutlinedIcon className="icon"/>
+                    <span>Delivery</span>
+                </li>
+                <p className="title">USEFUL</p>
+                <li>
+                    <QueryStatsOutlinedIcon className="icon"/>
+                    <span>Stats</span>
+                </li>
+                <li>
+                    <CircleNotificationsOutlinedIcon className="icon"/>
+                    <span>Notifications</span>
+                </li>
+                <p className="title">SERVICE</p>
+                <li>
+                    <FavoriteBorderOutlinedIcon className="icon"/>
+                    <span>System Health</span>
+                </li>
+                <li>
+                    < PsychologyOutlinedIcon className="icon"/>
+                    <span>Logs</span>
+                </li>
+                 <li>
+                     < SettingsBrightnessOutlinedIcon className="icon"/>
+                    <span>Settings</span>
+                </li>
+                <p className="title">USER</p>
+                 <li>
+                     < ManageAccountsOutlinedIcon className="icon"/>
+                    <span>Profile</span>
+                </li>
+                 <li>
+                     < LogoutOutlinedIcon className="icon"/>
+                    <span>Logout</span>
                 </li>
                 
             </ul>
         </div>
-        <div className="bottom">color option</div>
+        <div className="bottom">
+            <div className="colorOption"></div>
+             <div className="colorOption"></div>
+
+        </div>
     </div>
   )
 }
